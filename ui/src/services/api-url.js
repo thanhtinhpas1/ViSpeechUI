04fb6f2de1875be99d9e1b39c4b7dcb62ed4d180
Tran Khanh Linh <trankhanhlinh98@gmail.com>
2020-04-13 03:05:40 +0700

    fix react table css for admin UI

--- ui/src/services/api-url.js
+++ ui/src/services/api-url.js
@@ -3,6 +3,6 @@
 //     ? 'https://tutor-back-end-user.herokuapp.com'
 //     : 'http://localhost:4500'
 
-// export const apiUrl = 'http://localhost:7070/v1'
+export const apiUrl = 'http://localhost:7070/v1'
 export const socketUrl = 'http://asr.vietspeech.com:3200'
-export const apiUrl = 'http://asr.vietspeech.com:3200/api'
+// export const apiUrl = 'http://asr.vietspeech.com:3200/api'
