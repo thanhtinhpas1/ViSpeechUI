c09fca869afa975949b98b3fa825b5d1ebe60d4e
TinhTran <tinhtran@kms-technology.com>
2020-05-24 21:10:07 +0700

    update api url

--- ui/src/services/api-url.js
+++ ui/src/services/api-url.js
@@ -4,6 +4,6 @@
 //     : 'http://localhost:4500'
 
 // export const apiUrl = 'http://asr.vietspeech.com:3200/api'
-export const apiUrl = 'http://thanhtinh.cf:7070/v1'
+export const apiUrl = 'http://thanhtinh.cf:3200/api'
 // export const socketUrl = 'http://asr.vietspeech.com:3200'
 export const socketUrl = 'http://thanhtinh.cf:3200'
